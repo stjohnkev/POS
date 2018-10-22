fa45b3b4c5a66e18cd28da0c196314615cdf6831
stjohnkev <43213852+stjohnkev@users.noreply.github.com>
2018-10-22 19:53:37 +0100

    video 6

--- POS/app/src/main/java/e/kevin_st_john/pos/MainActivity.java
+++ POS/app/src/main/java/e/kevin_st_john/pos/MainActivity.java
@@ -1,22 +1,31 @@
 package e.kevin_st_john.pos;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mNameTextView, mQuantityTextView, mDateTextView;
     private Item mCurrentItem;
     private Item mClearedItem;
+    private ArrayList<Item> mItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
         mQuantityTextView = findViewById(R.id.quantity_text);
         mDateTextView=findViewById(R.id.date_text);
 
+        mItems.add(new Item("Example 1", 30, new GregorianCalendar()));
+        mItems.add(new Item("Example 2", 40, new GregorianCalendar()));
+        mItems.add(new Item("Example 3", 50, new GregorianCalendar()));
+
         //boilerplate
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -36,14 +49,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                mCurrentItem=Item.getDefaultItem();
-                showCurrentItem();
+               // mCurrentItem=Item.getDefaultItem();
+                //showCurrentItem();
+
+                addItem();
 
 
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                  //       .setAction("Action", null).show();
             }
         });
+    }
+
+    private void addItem() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //customise dialog
+        //builder.setMessage("hello");
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_add, null, false);
+        builder.setView(view);
+        final EditText namedEditText = view.findViewById(R.id.edit_name);
+        final EditText quantityEditText = view.findViewById(R.id.edit_quantity);
+        final CalendarView deliveryDateView = view.findViewById(R.id.calendar_view);
+        final GregorianCalendar calendar = new GregorianCalendar();
+
+        deliveryDateView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                calendar.set(year, month, dayOfMonth);
+            }
+        });
+
+        builder.setNegativeButton(android.R.string.cancel, null);
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //runs when u click ok
+                String name = namedEditText.getText().toString();
+                int quantity = Integer.parseInt(quantityEditText.getText().toString());
+                mCurrentItem = new Item(name, quantity, calendar);
+                showCurrentItem();
+                mItems.add(mCurrentItem);
+            }
+        });
+
+
+        builder.create().show();
     }
 
     private void showCurrentItem() {
@@ -83,6 +136,10 @@ public class MainActivity extends AppCompatActivity {
                 snackbar.show();
 
                 return true;
+            case R.id.action_search:
+                showSearchDialog();
+                return true;
+
             case R.id.action_settings:
                 startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
                 return true;
@@ -90,5 +147,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showSearchDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Choose an Item");
+        builder.setItems(getName(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mCurrentItem = mItems.get(which);
+                showCurrentItem();
+            }
+        });
+
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+    }
+
+
+    private String[] getName() {
+        String[] names = new String[mItems.size()];
+        for(int i=0;i<mItems.size();i++){
+            names[i] = mItems.get(i).getName();
+        }
+        return names;
     }
 }
